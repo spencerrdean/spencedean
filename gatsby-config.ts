ddeb846610f7c76b410979c4c6e497dc53815adc
Spencer Dean <srdean@wesleyan.edu>
2023-03-18 14:14:47 -0700

    base site customizations

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,14 +8,13 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.mjs
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
-    siteImage: `/banner.jpg`,
+    siteTitle: `Spencer Dean ✍️`,
+    siteTitleAlt: `Spencer Dean - Personal Site`,
+    siteHeadline: `Spencer Dean - Personal Site`,
+    siteUrl: `https://spencedean.com`,
+    siteDescription: `Technical writing, data science, and journalism portfolio.`,
     siteLanguage: `en`,
-    author: `@lekoarts_de`,
+    siteImage: `/banner.jpg`,
   },
   trailingSlash: `never`,
   plugins: [
@@ -25,8 +24,8 @@ const config: GatsbyConfig = {
       options: {
         navigation: [
           {
-            title: `Blog`,
-            slug: `/blog`,
+            title: `Work`,
+            slug: `/work`,
           },
           {
             title: `About`,
@@ -35,12 +34,12 @@ const config: GatsbyConfig = {
         ],
         externalLinks: [
           {
-            name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            name: `LinkedIn`,
+            url: `https://www.linkedin.com/in/spencer-russell-dean/`,
           },
           {
-            name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+            name: `Medium`,
+            url: `https://medium.com/@spencerdean`,
           },
         ],
       },
---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/footer.tsx
@@ -0,0 +1,39 @@
+/** @jsx jsx */
+import { jsx, Link } from "theme-ui"
+import useSiteMetadata from "../hooks/use-site-metadata"
+
+const Footer = () => {
+  const { siteTitle } = useSiteMetadata()
+
+  return (
+    <footer
+      sx={{
+        boxSizing: `border-box`,
+        display: `flex`,
+        justifyContent: `space-between`,
+        mt: [6],
+        color: `secondary`,
+        a: {
+          variant: `links.secondary`,
+        },
+        flexDirection: [`column`, `column`, `row`],
+        variant: `dividers.top`,
+      }}
+    >
+      <div>
+        &copy; {new Date().getFullYear()} by Spencer Dean.
+      </div>
+      <div>
+        <Link
+          aria-label="Link to the theme's GitHub repository"
+          sx={{ ml: [0, 0, 3] }}
+          href="https://github.com/spencerrdean/spencedean"
+        >
+          View source code on GitHub.
+        </Link>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
